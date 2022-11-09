0dc89e42e43a785e4e8f03733b70210dbc211145
saroar <saroar9@gmail.com>
2022-11-09 11:57:54 +0300

    add home page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,11 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+
+    app.get { req in
+        try await req.view.render("index")
+    }
+    
     try app.group("v1") { api in
         let chat = api.grouped("chat")
         let webSocketController = WebSocketController(eventLoop: app.eventLoopGroup.next(), db: app.db)
